8c64911043abe1c1aac0a7fa98b8a13280b338ad
Gonzalez <cgonzale@gccw2volbeat.igd.fraunhofer.de>
2020-10-12 20:38:42 +0200

    Change in test so that unm. masks are not stored

--- test/data/datasets/test_ds_mr_prostate_decathlon.py
+++ test/data/datasets/test_ds_mr_prostate_decathlon.py
@@ -1,13 +1,5 @@
 from mp.data.datasets.ds_mr_prostate_decathlon import DecathlonProstateT2
 
-def test_ds():
-    data = DecathlonProstateT2(merge_labels=False)
-    assert data.label_names == ['background', 'peripheral zone', 'central gland']
-    assert data.nr_labels == 3
-    assert data.modality == 'MR'
-    assert data.size == 32
-    assert data.name == 'DecathlonProstateT2'
-
 def test_ds_label_merging():
     data = DecathlonProstateT2(merge_labels=True)
     assert data.label_names == ['background', 'prostate']
