ae9fda597c360a265c59c2f30174f4d689b47485
memmelma <marius.memmel@gmx.net>
2020-12-20 17:15:35 +0100

    added argument parsing

---
+++ args.py
@@ -0,0 +1,65 @@
+import argparse
+import torch
+
+# parse train options
+
+def _get_parser():
+    parser = argparse.ArgumentParser()
+
+    # general
+    parser.add_argument('--experiment-name', type=str, default='', help='experiment name')
+    parser.add_argument('--nr-runs', type=int, default=1, help='# of runs')
+
+    # hardware
+    parser.add_argument('--device', type=str, default='cuda', help='device type cpu or cuda')
+    parser.add_argument("--device-ids", nargs="+", default=[0], type=int, help="ID(s) of GPU device(s)")
+
+    # dataset
+    parser.add_argument('--test-ratio', type=int, default=0.0, help='ratio of data to be used for testing')
+    parser.add_argument('--val-ratio', type=int, default=0.0, help='ratio of data to be used for validation')
+    parser.add_argument('--input_dim_c', type=int, default=1, help='input channels for images') 
+    parser.add_argument('--input_dim_hw', type=int, default=256, help='height and width for images')
+    parser.add_argument('--resize', action='store_true', help='specify if images should be resized')
+    parser.add_argument('--augmentation', type=str, default='none', help='augmentation to be used')
+    parser.add_argument('--n-samples', type=int, default=None, help='# of samples per dataloader, only use when debugging')
+
+    # training
+    parser.add_argument('--epochs', type=int, default=100, help='# of epochs')
+    parser.add_argument('--lr', type=int, default=1e-4, help='learning rate')
+    parser.add_argument('--batch-size', type=int, default=8, help='batch size')
+    parser.add_argument('--domain-code-size', type=int, default=3, help='# of domains')
+    parser.add_argument('--cross-validation', action='store_true', help='specify if cross validation should be used')
+
+    return parser
+
+def parse_args(argv):
+    """Parses arguments passed from the console as, e.g.
+    'python ptt/main.py --epochs 3' """
+
+    parser = _get_parser()
+    args = parser.parse_args(argv)
+    
+    args.device = str(args.device+':'+str(args.device_ids[0]) if torch.cuda.is_available() else "cpu")
+    device_name = torch.cuda.get_device_name(args.device)
+    print('Device name: {}'.format(device_name))
+
+    args.input_shape = (args.input_dim_c, args.input_dim_hw, args.input_dim_hw)
+    assert args.batch_size % 2 == 0, 'batch_size must be multiple of 2'
+
+    return args
+
+def parse_args_as_dict(argv):
+    """Parses arguments passed from the console and returns a dictionary """
+    return vars(parse_args(argv))
+
+def parse_dict_as_args(dictionary):
+    """Parses arguments given in a dictionary form"""
+    argv = []
+    for key, value in dictionary.items():
+        if isinstance(value, bool):
+            if value:
+                argv.append('--'+key)
+        else:
+            argv.append('--'+key)
+            argv.append(str(value))
+    return parse_args(argv)
--- disentangler_training.py
+++ disentangler_training.py
@@ -4,7 +4,10 @@
 # jupyter notebook. See that file for more detailed explainations.
 # ------------------------------------------------------------------------------
 
-# 1. Imports
+# Imports
+import sys
+from args import parse_args_as_dict
+from mp.utils.helper_functions import seed_all
 
 import torch
 import torch.nn as nn
@@ -30,33 +33,16 @@ from mp.models.disentangler.cmfd import CMFD
 # TODO: detect anomaly
 # torch.autograd.set_detect_anomaly(True)
 
-# 2. Define configuration
+# Get configuration from arguments
+config = parse_args_as_dict(sys.argv[1:])
+seed_all(42)
 
-config = {'experiment_name':'', 'device':'cuda', 'device_ids': (7),
-    'nr_runs': 1, 'cross_validation': False, 'val_ratio': 0.0, 'test_ratio': 0.1,
-    'input_shape': (1, 256, 256), 'resize': False, 'augmentation': 'none', 
-    'class_weights': (0.,1.), 'epochs': 50, 'lr': 1e-4, 'batch_size': 4, 'domain_code_size':3, 'n_samples': 5 # # samples per dataloader -> n_samples = None -> all data is used
-    }
+config['class_weights'] = (0., 1.)
 
-if type(config['device_ids']):
-    config['device_ids'] = [config['device_ids']]
-device = torch.device(config['device']+':'+str(config['device_ids'][0]) if torch.cuda.is_available() else "cpu")
-device_name = torch.cuda.get_device_name(device)
-print('Device name: {}'.format(device_name))
-
-assert config['batch_size'] % 2 == 0, 'batch_size has to be multiple of 2'
-
-# 3. Create experiment directories
+# Create experiment directories
 exp = Experiment(config=config, name=config['experiment_name'], notes='', reload_exp=True)
 
-# 4. Define data
-# data = Data()
-# data.add_dataset(DecathlonHippocampus(merge_labels=True))
-# nr_labels = data.nr_labels
-# label_names = data.label_names
-# train_ds = ('DecathlonHippocampus', 'train')
-# test_ds = ('DecathlonHippocampus', 'test')
-
+# Datasets
 data = Data()
 
 dataset_domain_a = DecathlonHippocampus(merge_labels=True)
@@ -77,14 +63,14 @@ train_ds_a = ('DecathlonHippocampus', 'train')
 train_ds_b = ('DryadHippocampus', 'train')
 # test_ds_c = ('HarP', 'test')
 
-# 5. Create data splits for each repetition
+# Create data splits for each repetition
 exp.set_data_splits(data)
 
 # Now repeat for each repetition
 for run_ix in range(config['nr_runs']):
     exp_run = exp.get_run(run_ix=0)
 
-    # 6. Bring data to Pytorch format and add domain_code
+    # Bring data to Pytorch format and add domain_code
     datasets = dict()
     for idx, item in enumerate(data.datasets.items()):
         ds_name, ds = item
@@ -96,52 +82,42 @@ for run_ix in range(config['nr_runs']):
                     ix_lst=data_ixs, size=config['input_shape']  , aug_key=aug, 
                     resize=config['resize'], domain_code=idx, domain_code_size=config['domain_code_size'])
 
-    # 6.1 combine datasets
-    # multi_domain_dataset = torch.utils.data.ConcatDataset((datasets[(train_ds_0)], datasets[(train_ds_1)]))
-
-    # TODO: maybe use ConcatDataset and draw double/split batch and cross domain samples
-    # i.e. batch of 8 -> two batches of 4
-    # transfer batch 0 to batch 1 and reconstruct
-    # 7. Build train dataloader, and visualize
-    # dl = DataLoader(multi_domain_dataset, 
-    #     batch_size=config['batch_size'], shuffle=True)
-
-    # 7. Build two train dataloaders -> two domains
-    # dl_a = DataLoader(datasets[(train_ds_a)], batch_size=config['batch_size'], shuffle=True)
-    # dl_b = DataLoader(datasets[(train_ds_b)], batch_size=config['batch_size'], shuffle=True)
-   
+    # Combine datasets
     multi_domain_dataset = torch.utils.data.ConcatDataset((datasets[(train_ds_a)], datasets[(train_ds_b)]))
     dl = DataLoader(multi_domain_dataset, batch_size=config['batch_size'], shuffle=True, drop_last=True)
 
-    # 8. Initialize model
-    model = CMFD(config['input_shape']  , latent_channels=256, domain_code_size=config['domain_code_size'], latent_scaler_sample_size=250)
-    model.to_device(device)
-    if len(config['device_ids']) > 1:
-        print('Using data parallel on devices', config['device_ids'])
-        model.parallel(device_ids=config['device_ids'])
+    # Initialize model
+    model = CMFD(config['input_shape'] , latent_channels=256, domain_code_size=config['domain_code_size'], latent_scaler_sample_size=250)
+    model.to_device(config['device'])
+    # if len(config['device_ids']) > 1:
+    #     print('Using data parallel on devices', config['device_ids'])
+        # model.parallel(device_ids=config['device_ids'])
+        # model = nn.DataParallel(model, device_ids=config['device_ids'])
 
-    # 9. Define loss and optimizer
-    loss_g = LossDiceBCE(bce_weight=1., smooth=1., device=device)
-    loss_f = LossClassWeighted(loss=loss_g, weights=config['class_weights'], 
-        device=device)
+    # Define loss and optimizer
+    loss_g = LossDiceBCE(bce_weight=1., smooth=1., device=config['device'])
+    loss_f = LossClassWeighted(loss=loss_g, weights=config['class_weights'], device=config['device'])
     # TODO: fix CELoss
     loss_c = torch.nn.CrossEntropyLoss()
+
+    # Set optimizers
     model.set_optimizers(optim.Adam, lr=config['lr'])
-    # 9.1 Set optimizer, pass None because optimizers are saved in model
+    # Set optimizer, pass None because optimizers are saved in model
     optimizer = None
 
-    # 10.1 Create tensorboard SummaryWriter
+    # Create tensorboard SummaryWriter
     writer = create_writer(config, exp.path)
-    # 10. Train model
+    
+    # Train model
     results = Result(name='training_trajectory')   
-    agent = DisentanglerAgent(model=model, label_names=label_names, device=device, summary_writer=writer)
+    agent = DisentanglerAgent(model=model, label_names=label_names, device=config['device'], summary_writer=writer)
     agent.train(results, optimizer, loss_g, dl,
         init_epoch=0, nr_epochs=config['epochs'], run_loss_print_interval=1,
         eval_datasets=datasets, eval_interval=5, 
         save_path=exp_run.paths['states'], save_interval=5,
         display_interval=1)
 
-    # 11. Save and print results for this experiment run
+    # Save and print results for this experiment run
     exp_run.finish(results=results, plot_metrics=['Mean_LossBCEWithLogits', 'Mean_LossDice[smooth=1.0]', 'Mean_LossCombined[1.0xLossDice[smooth=1.0]+1.0xLossBCEWithLogits]'])
     # test_ds_key = '_'.join(test_ds_c)
     # metric = 'Mean_LossDice[smooth=1.0]'
